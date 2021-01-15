bb10993ae8f3e1059d4ebc2d4e34eabf9c421acc
Krystof Zahumensky <zahumensky.krystof@gmail.com>
2021-01-15 19:18:26 +0100

    html design changes

---
+++ MongoFS/Data/Page.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoFS.Data
+{
+    public class Page
+    {
+        public Guid Id { get; set; }
+        public FileSystemType Type { get; set; }
+        public Page(Guid id, FileSystemType type)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+}
